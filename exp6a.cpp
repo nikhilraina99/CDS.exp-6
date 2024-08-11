8359070842d1b0399cc408f085a267c5b075aae9
nikhilraina99 <rainanikhilr@gmail.com>
2024-08-12 01:33:09 +0530

    Add files via upload

---
+++ exp6a.cpp
@@ -0,0 +1,23 @@
+//Nikhil
+//23070123093
+//entc b1
+//experiment 6a
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row; 
+    char a='A';
+    cout<< "Enter number of rows: ";
+    cin >> row;
+    for ( int i =0; i<row ; i++)
+    { for(int j= 0 ; j<= i; j++)
+    { 
+        cout << " "<<a;
+        a++;
+    }
+    
+    cout << endl;
+    }
+    return 0;
+}
---
+++ exp6b.cpp
@@ -0,0 +1,21 @@
+//Nikhil
+//23070123093
+//entc b1
+//experiment 6b
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum = 0, num;
+    cout << "Enter last number: ";
+    cin >> num;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+        
+
+    }
+    cout << "Sum is: "<<sum;
+    return 0;
+
+}
